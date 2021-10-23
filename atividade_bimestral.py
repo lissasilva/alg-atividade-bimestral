2cf91b3d3babae526487a6cf23fe9eb5c310cf0d
lissasilva <91398816+lissasilva@users.noreply.github.com>
2021-10-23 15:28:52 -0300

    Update atividade_bimestral.py

--- atividade_bimestral.py
+++ atividade_bimestral.py
@@ -94,6 +94,7 @@ def clientes_fun(clientes, listinha_dados):
         elif resposta_submenus == 3:
 
             teste2 = True
+            listinha_dados = []
             
             print()
             cpf = input('CPF: ')
@@ -268,19 +269,322 @@ def clientes_fun(clientes, listinha_dados):
 
                     else:
                         return()
+
+################################# FUNÇÃO VEÍCULOS ###########################################
             
 def veiculos_fun(veiculos, listinha_dados, opcionais):
-    
-    submenus()
-    
-    print()
-    resposta_submenus = int(input('Escolha uma opção: '))
 
+    resposta_submenus = 0
+
+    while resposta_submenus != 6 :
+        
+        submenus()
+        
+        print()
+        resposta_submenus = int(input('Escolha uma opção: '))
+
+        ################## OPÇÃO 6 - Sair #####################
+
+        if resposta_submenus == 6:
+                return()
+
+        ################## OPÇÃO 1 - Listar todos #####################
+
+        if resposta_submenus == 1:
+                
+            print()
+            codigo = input('Código: ')
+
+            if codigo in veiculos:
+
+                print()
+                print('Código:',codigo)
+                print('Descrição:',veiculos[codigo][0])
+                print('Categoria:',veiculos[codigo][1])
+                print('Combustível:',veiculos[codigo][2])
+                print('Ano:',veiculos[codigo][3])
+                print('Modelo:',veiculos[codigo][4])
+                print('Opcionais:',veiculos[codigo][5])
+
+            else:
+
+                print()
+                print('Código ainda não cadastrado!')
+
+        ################## OPÇÃO 2 - Listar um elemento específico #####################
+
+        elif resposta_submenus == 2:
+
+            codigo = input('Código: ')
+
+            if codigo in veiculos:
+                    
+                print()
+                print('--------------')
+                print('1. Descrição')
+                print('2. Categoria')
+                print('3. Combustível')
+                print('4. Ano')
+                print('5. Modelo')
+                print('6. Opcionais')
+                print('--------------')
+
+                print()
+                resposta_sub_sub = int(input('Escolha uma opção: '))
+
+                if resposta_sub_sub == 1:
+                        
+                    print()
+                    print('Descrição:', veiculos[codigo][0])
+
+                elif resposta_sub_sub == 2:
+
+                    print()
+                    print('Categoria:', veiculos[codigo][1])
+
+                elif resposta_sub_sub == 3:
+
+                    print()
+                    print('Combustível:', veiculos[codigo][2])
+
+                elif resposta_sub_sub == 4:
+                    
+                    print()
+                    print('Ano:', veiculos[codigo][3])
+
+                elif resposta_sub_sub == 5:
+                        
+                    print()
+                    print('Modelo:', veiculos[codigo][4])
+                        
+                else:
+                        
+                    print()
+                    print('Opcionais:', veiculos[codigo][5])
+
+            else:
+
+                print()
+                print('CPF ainda não cadastrado!')                    
+
+        ################## OPÇÃO 3 - Incluir #####################
+
+        elif resposta_submenus == 3:
+
+            listinha_dados = []
+            opcionais = []
+            
+            teste = True
+                
+            print()
+            codigo = input('Código: ')
+
+            while teste != False:
+                
+                if codigo in veiculos:
+                    
+                    print()
+                    print('Código já cadastrado, digite novamente')
+                    print()
+                    codigo = input('Código: ')
+                    
+                else:
+                    
+                    teste = False
+
+                  
+            Descrição = input('Digite a descrição: ')
+            Categoria = input('Digite a categoria: ')
+            Combustível = input('Digite o combustível: ')
+            Ano = input('Digite o ano: ')
+            Modelo = input('Digite o modelo: ')
+
+            respostas_opcionais = input('Quer adicionar mais informação?: (S/N) ')
+
+            while respostas_opcionais == 'S':
+                      
+                opcional = input('Digite a informação: ')
+                opcionais.append(opcional)
+
+                respostas_opcionais = input('Quer adicionar mais informação?: (S/N) ')
+                      
+            listinha_dados.append(Descrição)
+            listinha_dados.append(Categoria)
+            listinha_dados.append(Combustível)
+            listinha_dados.append(Ano)
+            listinha_dados.append(Modelo)
+            listinha_dados.append(opcionais)
+
+            veiculos[codigo] = listinha_dados
+
+            print()
+            print(veiculos)
+                
+        ################## OPÇÃO 4 - Alterar - OPÇÃO AINDA NÃO EDITADA! #####################
+
+        elif resposta_submenus == 4:
+                
+            cpf = input('CPF: ')
+
+            if cpf in clientes:
+                    
+                print()
+                print('-------------')
+                print('1. Nome')
+                print('2. Endereço')
+                print('3. Telefone')
+                print('4. Nascimento')
+                print('-------------')
+
+                print()
+                resposta_sub_sub = int(input('Escolha uma opção: '))
+
+                ##
+                if resposta_sub_sub == 1:
+                        
+                    nome = input('Nome a atualizar: ')
+
+                    confirmação = input('Confirme a atualização (S/N): ')
+
+                    if confirmação == 'S':
+
+                        clientes[cpf][0] = nome
+                        print(clientes)
+
+                    else:
+                        return()
+                    
+                ##
+                elif resposta_sub_sub == 2:
+
+                    endereço = input('Endereço a atualizar: ')
+
+                    confirmação = input('Confirme a atualização (S/N): ')
+
+                    if confirmação == 'S':
+
+                        clientes[cpf][1] = endereço
+                        print(clientes)
+
+                    else:
+                        
+                        return()
+
+                ##
+                elif resposta_sub_sub == 3:
+
+                    telefone = input('Telefone a atualizar: ')
+
+                    confirmação = input('Confirme a atualização (S/N): ')
+
+                    if confirmação == 'S':
+
+                        clientes[cpf][2] = telefone
+                        print(clientes)
+
+                    else:
+                            
+                        return()
+
+                ##
+                else:
+
+                    nascimento = input('Nascimento a atualizar: ')
+
+                    confirmação = input('Confirme a atualização (S/N): ')
+
+                    if confirmação == 'S':
+
+                        clientes[cpf][3] = nascimento
+                        print(clientes)
+
+                    else:
+                        
+                        return()
+                    
+            else:
+
+                print()
+                print('CPF ainda não cadastrado!')
+
+        ################## OPÇÃO 5 - Excluir - OPÇÃO AINDA NÃO EDITADA! #####################
+                    
+        else:
+
+                cpf = input('CPF: ')
+
+                if cpf in clientes:
+                    
+                    print()
+                    print('-------------')
+                    print('1. Nome')
+                    print('2. Endereço')
+                    print('3. Telefone')
+                    print('4. Nascimento')
+                    print('-------------')
+
+                    print()
+                    resposta_sub_sub = int(input('Escolha uma opção: '))
+
+                    if resposta_sub_sub == 1:
+
+                        confirmação = input('Confirme a retirada (S/N): ')
+
+                        if confirmação == 'S':
+                                
+                            atualização = 'sem dados'
+                            clientes[cpf][0] = atualização
+                            print(clientes)
+
+                        else:
+                            return()
+                    
+                    elif resposta_sub_sub == 2:
+
+                        confirmação = input('Confirme a retirada (S/N): ')
+
+                        if confirmação == 'S':
+                                
+                            atualização = 'sem dados'
+                            clientes[cpf][1] = atualização
+                            print(clientes)
+
+                        else:
+                            return()
+
+                    elif resposta_sub_sub == 3:
+
+                        confirmação = input('Confirme a retirada (S/N): ')
+
+                        if confirmação == 'S':
+                                
+                            atualização = 'sem dados'
+                            clientes[cpf][2] = atualização
+                            print(clientes)
+
+                        else:
+                            return()
+
+                    else:
+
+                        confirmação = input('Confirme a retirada (S/N): ')
+
+                        if confirmação == 'S':
+                                
+                            atualização = 'sem dados'
+                            clientes[cpf][3] = atualização
+                            print(clientes)
+
+                        else:
+                            return()
+
+#################################### FUNÇÃO ALUGUÉIS ############################################
     
 def alugueis_fun(alugueis, listinha_dados):
     
     submenus()
 
+#################################### FUNÇÃO RELATÓRIOS ############################################
 
 def relatorios_fun(clientes, veiculos, relatorios):
     print()
